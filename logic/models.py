7c08532e67847c8d17a1c6efcdf93a98d8a0c164
frednboria <lang@ensta.fr>
2013-04-27 14:36:35 +0200

    Création du modèle "Demonstration" et d'une bdd sqlite

--- logic/models.py
+++ logic/models.py
@@ -1,3 +1,16 @@
+#-*- coding: utf-8 -*-
 from django.db import models
-
-# Create your models here.
+ 
+class Demonstration(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=42)
+    content = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+ 
+    def __unicode__(self):
+        """ 
+        Cette méthode que nous définirons dans tous les modèles
+        nous permettra de reconnaître facilement les différents objets que nous
+        traiterons plus tard et dans l'administration
+        """
+    	return u"%s" % self.title
